ad6cf19e6c4b62f8e0c0df7b3c12a69433e174f7
oybek003 <@ojbekmuslimov1.com>
2025-07-19 16:13:50 +0500

    fff

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // 👈 очень важно!
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tailwindcss()],
+})
+
+
